bd5228c3c55ddb5af97c13b10c19c4ba0acc653a
Lucas Santos <lucas.hilleshein.ext@sodexo.com>
2022-05-29 16:25:53 -0300

    chore: add ReadingType entity

---
+++ src/main/java/br/com/lucashilles/domains/reading/readingtype/ReadingType.java
@@ -0,0 +1,23 @@
+package br.com.lucashilles.domains.reading.readingtype;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "sys_reading_type")
+public class ReadingType extends PanacheEntity {
+
+    @Column(name = "name")
+    public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
