ad95ac7cb7cb2027e5fe0bb2e2d35d29fa2ed2e3
Lucas HS <lucashilles@gmail.com>
2022-11-12 19:10:40 -0300

    chore(Reading): create basic crud operations

--- src/main/java/br/com/lucashilles/domains/reading/ReadingController.java
+++ src/main/java/br/com/lucashilles/domains/reading/ReadingController.java
@@ -1,13 +1,11 @@
 package br.com.lucashilles.domains.reading;
 
-import io.vertx.core.json.JsonObject;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 import org.jboss.resteasy.annotations.jaxrs.QueryParam;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
@@ -17,6 +15,39 @@ public class ReadingController {
 
     @Inject
     ReadingService readingService;
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public void deleteReading(@PathParam long id) {
+        readingService.deleteReading(id);
+    }
+
+    @GET
+    public List<ReadingDto> getReadings (
+            @QueryParam Long patient,
+            @QueryParam("reading-type") Long readingType,
+            @QueryParam String from,
+            @QueryParam String to,
+            @QueryParam String date
+    ) throws Exception {
+        if (date != null && (from != null || to != null)) {
+            throw new Exception("Date can only be use alone");
+        }
+        return null;
+    }
+
+    @POST
+    @Transactional
+    public Reading createReading(Reading reading) {
+        return readingService.createReading(reading);
+    }
+
+    @PUT
+    @Transactional
+    public Reading updateReading(Reading reading) {
+        return readingService.updateReading(reading);
+    }
 
     @GET
     @Path("/{id}")
@@ -28,5 +59,19 @@ public class ReadingController {
     @Path("/all")
     public List<ReadingDto> getAllByDate(@QueryParam String date) {
         return readingService.getAllByDate(date);
+    }
+
+    @GET
+    @Path("/latest")
+    public List<ReadingDto> getLatest(@QueryParam Long patient, @QueryParam("reading-type") Long readingType) {
+        if (patient == null && readingType == null) {
+            return readingService.getAllLatest();
+        } if (readingType == null) {
+            return readingService.getAllLatestByPatient(patient);
+        } if (patient == null) {
+            return readingService.getAllLatestByType(readingType);
+        }
+
+        return readingService.getLatestByPatientAndType(patient, readingType);
     }
 }
--- src/main/java/br/com/lucashilles/domains/reading/ReadingService.java
+++ src/main/java/br/com/lucashilles/domains/reading/ReadingService.java
@@ -1,14 +1,102 @@
 package br.com.lucashilles.domains.reading;
 
+import org.hibernate.Session;
+
 import javax.enterprise.context.ApplicationScoped;
-import javax.persistence.EntityManager;
+import javax.inject.Inject;
 import java.util.List;
 
 @ApplicationScoped
 public class ReadingService {
 
+    @Inject
+    Session session;
+
+    public Reading createReading(Reading reading) {
+        reading.persist();
+        return reading;
+    }
+
+    public void deleteReading(long id) {
+        Reading reading = Reading.findById(id);
+        if (reading != null) {
+            reading.delete();
+        }
+    }
+
+    public Reading updateReading(Reading reading) {
+        reading.persist();
+        return reading;
+    }
+
     public List<ReadingDto> getAllByDate(String date) {
         String query = "DATE(date) = DATE(?1)";
         return Reading.find(query, date).project(ReadingDto.class).list();
+    }
+
+    public List<ReadingDto> getAllLatest() {
+        String query = "SELECT r.*" +
+                "FROM sys_reading r" +
+                "JOIN (" +
+                "    SELECT MAX(date) AS date, reading_type_id, patient_id" +
+                "    FROM sys_reading" +
+                "    GROUP BY patient_id, reading_type_id" +
+                ") f" +
+                "ON r.date = f.date" +
+                "AND r.reading_type_id = f.reading_type_id" +
+                "AND r.patient_id = f.patient_id;";
+
+        return session.createNativeQuery(query, ReadingDto.class).list();
+    }
+
+    public List<ReadingDto> getAllLatestByPatient(Long patientId) {
+        String query = "SELECT r.*" +
+                "FROM sys_reading r" +
+                "JOIN (" +
+                "    SELECT MAX(date) AS date, reading_type_id, patient_id" +
+                "    FROM sys_reading AS f" +
+                "    WHERE patient_id = :patientId" +
+                "    GROUP BY reading_type_id, patient_id" +
+                ") f" +
+                "ON r.date = f.date" +
+                "AND r.reading_type_id = f.reading_type_id;";
+
+        return session.createNativeQuery(query, ReadingDto.class).setParameter("patientId", patientId).list();
+    }
+
+    public List<ReadingDto> getAllLatestByType(Long readingTypeId) {
+        String query = "SELECT r.*" +
+                "FROM sys_reading r" +
+                "JOIN (" +
+                "    SELECT MAX(date) AS date, reading_type_id, patient_id" +
+                "    FROM sys_reading" +
+                "    WHERE reading_type_id = :readingTypeId" +
+                "    GROUP BY patient_id, reading_type_id" +
+                ") f" +
+                "ON r.date = f.date" +
+                "AND r.reading_type_id = f.reading_type_id" +
+                "AND r.patient_id = f.patient_id;";
+
+        return session.createNativeQuery(query, ReadingDto.class).setParameter("readingTypeId", readingTypeId).list();
+    }
+
+    public List<ReadingDto> getLatestByPatientAndType(Long patientId, Long readingTypeId) {
+        String query = "SELECT r.*" +
+                "FROM sys_reading r" +
+                "JOIN (" +
+                "    SELECT MAX(date) AS date, reading_type_id, patient_id" +
+                "    FROM sys_reading" +
+                "    WHERE patient_id = :patientId"+
+                "       AND reading_type_id = :readingTypeId" +
+                "    GROUP BY patient_id, reading_type_id" +
+                ") f" +
+                "ON r.date = f.date" +
+                "AND r.reading_type_id = f.reading_type_id" +
+                "AND r.patient_id = f.patient_id;";
+
+        return session.createNativeQuery(query, ReadingDto.class)
+                .setParameter("patientId", patientId)
+                .setParameter("readingTypeId", readingTypeId)
+                .list();
     }
 }
