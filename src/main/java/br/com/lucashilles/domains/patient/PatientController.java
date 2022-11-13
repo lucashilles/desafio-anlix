3a52548bdb531a14fade4a1ff1539257fd65842e
Lucas HS <lucashilles@gmail.com>
2022-11-13 16:25:33 -0300

    chore(Patient): rename service methods

--- src/main/java/br/com/lucashilles/domains/patient/PatientController.java
+++ src/main/java/br/com/lucashilles/domains/patient/PatientController.java
@@ -10,6 +10,7 @@ import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/patient")
+@Produces(MediaType.APPLICATION_JSON)
 public class PatientController {
 
     @Inject
@@ -19,35 +20,78 @@ public class PatientController {
     @Consumes(MediaType.APPLICATION_JSON)
     @Transactional
     public Patient createPatient(Patient newPatient) {
-        return patientService.createPatient(newPatient);
+        return patientService.create(newPatient);
     }
 
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     @Transactional
     public Patient updatePatient(Patient patient) {
-        return patientService.updatePatient(patient);
+        return patientService.update(patient);
     }
 
     @DELETE
     @Path("/{id}")
     @Transactional
     public void deleteById(@PathParam long id) {
-        patientService.deletePatient(id);
+        patientService.delete(id);
     }
 
     @GET
     @Path("/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
     public Patient getById(@PathParam long id) {
         return Patient.findById(id);
     }
 
     @GET
     @Path("/find")
-    @Produces(MediaType.APPLICATION_JSON)
     public List<Patient> findPatient(@QueryParam String name) {
         return Patient.list("LOWER(name) LIKE ?1", "%" + name.toLowerCase() + "%");
     }
 
+//    @GET
+//    @Path("/{id}/status")
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public List<ReadingDto> getLastReadings(@PathParam long id) {
+//        String query = "SELECT  " +
+//                "    r.*  " +
+//                "FROM public.sys_reading AS r  " +
+//                "JOIN (  " +
+//                "    SELECT MAX(gp.date) AS date, gp.reading_type_id  " +
+//                "    FROM public.sys_reading AS gp  " +
+//                "    WHERE gp.patient_id = :id  " +
+//                "    GROUP BY gp.reading_type_id  " +
+//                ") AS gp ON r.date = gp.date AND r.reading_type_id = gp.reading_type_id";
+//
+//        return session.createNativeQuery(query, Reading.class).setParameter("id", id).list();
+//    }
+
+//    @GET
+//    @Path("/{id}/last-readings")
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public List<Reading> getLastReadings(@PathParam long id) {
+//        String query = "SELECT  " +
+//                "    r.*  " +
+//                "FROM public.sys_reading AS r  " +
+//                "JOIN (  " +
+//                "    SELECT MAX(gp.date) AS date, gp.reading_type_id  " +
+//                "    FROM public.sys_reading AS gp  " +
+//                "    WHERE gp.patient_id = :id  " +
+//                "    GROUP BY gp.reading_type_id  " +
+//                ") AS gp ON r.date = gp.date AND r.reading_type_id = gp.reading_type_id";
+//
+//        return session.createNativeQuery(query, Reading.class).setParameter("id", id).list();
+//    }
+
+
+//    SELECT *
+//    FROM sys_reading r
+//    JOIN (
+//      SELECT max(date) AS date, reading_type_id, patient_id
+//      FROM sys_reading
+//      GROUP BY patient_id, reading_type_id
+//    ) f
+//    ON r.date = f.date
+//    AND r.reading_type_id = f.reading_type_id
+//    AND r.patient_id = f.patient_id
 }
--- src/main/java/br/com/lucashilles/domains/patient/PatientService.java
+++ src/main/java/br/com/lucashilles/domains/patient/PatientService.java
@@ -6,7 +6,7 @@ import javax.persistence.EntityExistsException;
 @ApplicationScoped
 public class PatientService {
 
-    public Patient createPatient(Patient patient) throws EntityExistsException {
+    public Patient create(Patient patient) throws EntityExistsException {
         if (Patient.find("cpf = ?1", patient.cpf) == null) {
             patient.persist();
             return patient;
@@ -15,17 +15,17 @@ public class PatientService {
         throw new EntityExistsException();
     }
 
-    public void deletePatient(Long id) {
+    public void delete(Long id) {
         Patient patient = Patient.findById(id);
         if (patient != null) {
             patient.delete();
         }
     }
 
-    public Patient updatePatient(Patient patient) throws EntityExistsException {
+    public Patient update(Patient patient) throws EntityExistsException {
         if (Patient.findById(patient.id) == null) {
             patient.id = null;
-            return createPatient(patient);
+            return create(patient);
         }
 
         return Patient.getEntityManager().merge(patient);
